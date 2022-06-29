b0987d878cacbc8e0fcc1dc657635256d6d07da4
Kirill <FuckingSherlock@users.noreply.github.com>
2022-06-30 01:13:53 +0300

    upd

--- main.js
+++ main.js
@@ -3,32 +3,32 @@ const products = {
         {
             name: 'Хлеб',
             price: 40,
-            count: 4,
+            count: 0,
         },
         {
             name: 'Футболка',
             price: 300,
-            count: 3,
+            count: 0,
         },
         {
             name: 'Очки',
             price: 500,
-            count: 1,
+            count: 0,
         },
         {
             name: 'Молоко',
             price: 80,
-            count: 1,
+            count: 0,
         },
         {
             name: 'Сидр',
             price: 90,
-            count: 3,
+            count: 0,
         },
         {
             name: 'Сникерс',
             price: 70,
-            count: 2,
+            count: 0,
         },
     ],
 
@@ -50,6 +50,7 @@ const products = {
 };
 
 
+
 function update() {
     console.log(products.getTotalPrice());
     let basket = document.getElementById('monitor');
@@ -64,14 +65,44 @@ function update() {
 
 update()
 
-function buy(product) {
+let bul = 0
+function buy(product, bul) {
     for (var i in products.items) {
         if (products.items[i].name == product) {
-            products.items[i].count += 1;
+            if (bul == 0) { products.items[i].count += 1; }
+            else if (products.items[i].count > 0) {
+                products.items[i].count -= 1;
+            }
             break
         }
     }
-    update()
+    update(); viever()
 }
 
-console.log(products.items)
+function viever() {
+    let viev = document.getElementById('viev')
+    viev.innerHTML = '<h3>Сотав корзины</><br/>'
+    for (var i in products.items) {
+        viev.innerHTML += products.items[i].name + "<br/>" + " Цена за шт.  " + products.items[i].price + " Количество в корзине   " + products.items[i].count + "<br/><br/>";
+    }
+}
+
+viever()
+
+function getButtons() {
+    let buttons = document.getElementById('product-list')
+    buttons.innerHTML = '<br/>'
+    for (var i in products.items) {
+        let btn = document.createElement('A')
+        let btn2 = document.createElement('A')
+        btn.innerHTML += `<button>Добавить ${products.items[i].name}</button>`;
+        btn.setAttribute('onclick', `buy('${products.items[i].name}', 0)`)
+        buttons.appendChild(btn);
+        btn2.innerHTML += `<button>Убрать ${products.items[i].name}</button>` + '<br /><br />';
+        btn2.setAttribute('onclick', `buy('${products.items[i].name}', 1)`)
+        buttons.appendChild(btn2);
+    }
+}
+
+
+getButtons()
